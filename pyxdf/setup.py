753401dfcc40302efd09bdd0f9e6b576d3f46c47
micuat <micuat@gmail.com>
2017-06-14 05:59:43 -0400

    python package

---
+++ pyxdf/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyxdf',
+      version='0.1',
+      description='Python library for importing XDF (Extensible Data Format)',
+      url='http://github.com/sccn/xdf',
+      author='Chadwick Boulay',
+      author_email='chadwick.boulay@gmail.com',
+      license='MIT',
+      packages=['pyxdf'],
+      zip_safe=False)
