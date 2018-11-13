030c411a339fd1482c22bd65267466dfec64ddcb
Christian Kothe <christian.kothe@gmail.com>
2018-11-13 12:07:48 -0800

    Another fix to the old copyright line

--- Python/xdf.py
+++ Python/xdf.py
@@ -137,7 +137,6 @@ def load_xdf(filename,
         This file is covered by the BSD license.
 
         Copyright (c) 2015-2018, Syntrogi Inc. dba Intheon
-        All rights reserved.
 
         Redistribution and use in source and binary forms, with or without
         modification, are permitted provided that the following conditions are
